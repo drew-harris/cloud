e20de9c58a2dfb02ca5843f36375f755bb1c28fb
Drew Harris <harrisd@smu.edu>
2024-05-19 15:05:33 -0500

    Center sign up form

--- site/src/routes/_auth.tsx
+++ site/src/routes/_auth.tsx
@@ -2,7 +2,7 @@ import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth")({
   component: () => (
-    <div className="justify-items-center items-center m-auto pt-40 px-8 md:px-0 md:pt-80">
+    <div className="justify-items-center md:h-screen flex items-center m-auto pt-40 md:pt-0 px-8 md:px-0">
       <Outlet />
     </div>
   ),
