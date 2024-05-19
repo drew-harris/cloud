7c02f89e7c58110bec1a1db04de4f5ede0a8fe73
Drew Harris <harrisd@smu.edu>
2024-05-19 13:47:53 -0500

    Post shadcn setup

--- site/twconfig.ts
+++ site/twconfig.ts
@@ -1,8 +1,13 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config = {
   darkMode: ["class"],
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -31,3 +36,6 @@ export default {
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
+
+export default config;
+
