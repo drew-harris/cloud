cd548739c88d3415eae10552e4ca74cd7de173fb
Drew Harris <harrisd@smu.edu>
2024-05-19 13:29:04 -0500

    Pre shadcn setup

--- site/src/routes/_auth/login.tsx
+++ site/src/routes/_auth/login.tsx
@@ -1,14 +1,29 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { DitherBox } from "~/components/DitherBox";
 
 export const Route = createFileRoute("/_auth/login")({
   component: () => {
+    const [codeInput, setCodeInput] = useState("");
+
     return (
-      <a
-        href="/auth/login"
-        className="mx-auto max-w-[200px] text-center halftone-d-4 bg-black text-white border-black p-2 halftone-offset block halftone-shadow"
+      <DitherBox
+        className="flex max-w-sm p-2 mx-auto"
+        title="Log In With Beta Code"
       >
-        Log In With Github
-      </a>
+        <a
+          href="/auth/login"
+          className="mx-auto max-w-[200px] text-center halftone-d-4 bg-black text-white border-black p-2 halftone-offset block halftone-shadow"
+        >
+          Log In With Github
+        </a>
+        <a
+          href="/auth/login"
+          className="mx-auto max-w-[200px] text-center halftone-d-4 bg-black text-white border-black p-2 halftone-offset block halftone-shadow"
+        >
+          Log In With Github
+        </a>
+      </DitherBox>
     );
   },
 });
